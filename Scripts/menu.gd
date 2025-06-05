7f9b566e6a279f8c1212a48f3d4fd32bb556c479
orion <louiseportela@edu.unifor.br>
2025-06-05 18:28:51 -0300

    UI

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -1,12 +1,15 @@
 extends Control
 
-@onready var buttons = [$StartGame,$QuitGame]
+@onready var start_game: Button = %StartGame
+@onready var quit_game: Button = %QuitGame
+
+@onready var buttons = [start_game,quit_game]
 var selected = 0
 
 var controller = false
 
 func _on_start_game_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/cutscenes.tscn")
+	pass
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
 @onready var main = $"../."
-@onready var buttons = [$ResumeGame,$QuitGame]
+@onready var resume_game: Button = %ResumeGame
+@onready var quit_game: Button = %QuitGame
+@onready var buttons = [resume_game,quit_game]
 var selected = 0
 
 func _process(delta: float) -> void:
@@ -9,32 +11,14 @@ func _process(delta: float) -> void:
 		if Input.is_action_just_pressed("down"):
 			if selected + 1 < len(buttons):
 				selected += 1
-				focus_bt(selected)
 		if Input.is_action_just_pressed("up"):
 			if selected - 1 >= 0:
 				selected -= 1
-				focus_bt(selected)
 		if Input.is_action_just_pressed("skip"):
-			select_bt()
+			pass
 
 func _on_resume_game_pressed() -> void:
 	main.toggle_pause()
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
-
-func _on_visibility_changed() -> void:
-	if visible:
-		focus_bt(0)
-
-func focus_bt(bt):
-	for i in range(len(buttons)):
-		if bt == i:
-			buttons[i].button_pressed = true
-		else:
-			buttons[i].button_pressed = false
-
-func select_bt():
-	for i in range(len(buttons)):
-		if buttons[i].button_pressed:
-			buttons[i].emit_signal("pressed")
